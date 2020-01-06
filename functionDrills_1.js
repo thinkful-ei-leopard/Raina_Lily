568ba5cff9e57f1d0ec26026922bd191b1aaf132
Raina Shin <yewon95@vt.edu>
2020-01-06 18:28:21 -0500

    step 7

--- functionDrills_1.js
+++ functionDrills_1.js
@@ -1,19 +1,23 @@
 'use strict';
 
 function getYearOfBirth(age){
-    
     return 2020 - age;
   }
 
-
-
-function createGreeting(name, age) {
-    const yearOfBirth = 2020 - age; 
-    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
+  function yearOfBirth(age) {
+      if (age < 0) {
+          throw new Error("Age cannot be negative");
+      return age;   
+      }
+  }
+  function createGreeting(name, age) {
+    const Birth = 2020 - age; 
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${Birth}`;
 }
 
-
-
-
-const greeting1 = createGreeting();
-console.log(greeting1);
+try {
+    const greeting1 = createGreeting('Raina', 24);
+    console.log(greeting1);
+} catch(e) {
+    console.error(e.message);
+}
