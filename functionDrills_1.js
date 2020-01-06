193cd6c5600c821de3f7352012ddebe691cef847
Raina Shin <yewon95@vt.edu>
2020-01-06 18:21:30 -0500

    step 6

--- functionDrills_1.js
+++ functionDrills_1.js
@@ -1,9 +1,19 @@
 'use strict';
 
+function getYearOfBirth(age){
+    
+    return 2020 - age;
+  }
+
+
+
 function createGreeting(name, age) {
-    const yearOfBirth = 2020 - age;
+    const yearOfBirth = 2020 - age; 
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
 }
+
+
+
 
 const greeting1 = createGreeting();
 console.log(greeting1);
